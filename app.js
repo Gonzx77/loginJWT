57fd885a5c4eff7af7f29959e285174924d2fd4b
Gonzx77 <160796511+Gonzx77@users.noreply.github.com>
2024-10-30 16:11:41 -0500

    feat: :wrench: URL was changed

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 passport.use(new DiscordStrategy({
     clientID: process.env.DISCORD_CLIENT_ID,
     clientSecret: process.env.DISCORD_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/discord/callback',
+    callbackURL: 'https://login-jwt-rho.vercel.app/auth/discord/callback',
     scope: ['identify', 'email']
 },
     (accessToken, refreshToken, profile, done) => {
@@ -52,7 +52,7 @@ passport.use(new DiscordStrategy({
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: "https://login-jwt-rho.vercel.app/auth/google/callback",
     scope: ['profile', 'email'] // Asegúrate de agregar esto
 },
 (accessToken, refreshToken, profile, done) => {
@@ -79,7 +79,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_CLIENT_ID,
     clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/callback",
+    callbackURL: "https://login-jwt-rho.vercel.app/auth/facebook/callback",
     profileFields: ['id', 'displayName', 'emails', 'photos']
 },
 (accessToken, refreshToken, profile, done) => {
